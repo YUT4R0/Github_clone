db0ff2f0d1adba9913ce1a9a96a9d6d9f04338a4
Yutaro <pedroyutaronakamura@gmail.com>
2023-02-24 00:33:59 -0400

    setted initial structures

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Profile from './pages/Profile';
+import Repo from './pages/Repo';
+
+function App() {
+  return(
+    <BrowserRouter>
+
+      <Header />
+      
+      <Routes>
+        <Route path="/" element={<Profile />} />
+        <Route path="/:username" element={<Profile />} />
+        <Route path="/:username/:reponame" element={<Repo />} />
+      </Routes>
+
+      <Footer />
+
+    </BrowserRouter>
+  )
+}
+
+export default App;
---
+++ src/pages/Repo/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container } from "./styles";
+
+const Repo = () => {
+  return (
+    <Container>
+      
+    </Container>
+  );
+}
+
+export default Repo
